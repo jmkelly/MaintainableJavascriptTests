ce2978fd4bf871b6ac204de41246fccf5953e1fa
James Kelly <james@midway-pl.com.au>
2012-07-13 16:52:06 +1000

    refactor so it all passes jshint

--- MaintainableJavascriptTests/assets/js/code.js
+++ MaintainableJavascriptTests/assets/js/code.js
@@ -1,106 +1,156 @@
-﻿var Math = {
-    add: function (a, b) {
+﻿var App = {};
+var Utils = {};
+var Application = {};
+var Errors = {};
+var SecondApplication = {};
+
+//A simple namespaced function to divide and mutiply
+(function () {
+    "use strict";
+  
+    Utils.multiply = function (a, b) {
+        return a * b;
+    },
+    Utils.divide = function (a, b) {
+        return a / b;
+    };
+} ()
+);
+
+//Show that via namespacing, we can implement the same functions without fear.
+(function () {
+    "use strict";
+    Utils.add = function (a, b) {
         return a + b;
-    }
-}
+    },
+    Utils.multiply = function (a, b) {
+        return a * b;
+    },
+    Utils.divide = function (a, b) {
+        return a / b;
+    },
 
-var Application = {
-
-    Math: {
-        add: function(a, b) {
+    Utils.Math = {
+        add: function (a, b) {
             return a + b;
         },
         subtract: function (a, b) {
             return a - b;
+        },
+        multiply:  function (a, b) {
+            return a * b;
+        },
+        divide:  function (a, b) {
+            return a / b;
         }
-    },
-    Person:
-    {
-        name: "James",
-        sayName: function() {
-            return this.name;
+    };
+} ()
+);
+
+//Define an application namespace and nested object within it.
+(function () {
+    "use strict";
+    Application.Math =  {
+            add: function (a, b) {
+                return a + b;
+            },
+            subtract: function (a, b) {
+                return a - b;
+            }
+        },
+
+        Application.Person = 
+        {
+            name: "James",
+            sayName: function () {
+                return this.name;
+            }
+        },
+
+        Application.Book = function (name) {
+            this.authors = {};
+            this.name = name;
+            this.fixedId = 15;
+        },
+
+        Application.BookWrapper = function (book) {
+            this.book = book;
+        },
+
+
+        Application.HeavyBook = function (name) {
+            this.Book.Call(this, name);
+        },
+
+        Application.Comparison = {
+            isArray: function (value) {
+                return typeof value.sort === "function";
+            }
+        },
+
+        Application.checkForProperty = function (object, property) {
+            if (property in object) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+
+        Application.invalidRequest = function (config) {
+            return config.MSG_INVALID_VALUE;
+        },
+
+        Application.throwAnError = function () {
+            throw new Error("This is a custom Exception");
+        },
+
+        Application.throwACustomError = function () {
+            Errors.CustomError.prototype = new Error();
+            throw new Errors.CustomError("My custom error");
+        };
+    }()
+);
+
+//using the Application namespace to define config settings.
+(function () {
+    "use strict";
+    Application.config = {
+        MSG_INVALID_VALUE: "Invalid value",
+        URL_INVALID: "/errors/invalid/",
+        CSS_SELECTED: "selected"
+    };
+}()
+);
+
+(function () {
+    "use strict";
+    Errors = {
+        CustomError: function CustomError(message) {
+            this.message = message;
         }
-    },
+    };
+}()
+);
 
-
-    Book: function(name) {
-        this.authors = {};
-        this.name = name,
-        this.fixedId = 15
-    },
-
-    BookWrapper: function(book) {
-        this.book = book;
-    },
-
-
-    HeavyBook: function(name) {
-        Book.Call(this, name);
-    },
-
-    Comparison: {
-        isArray: function(value) {
-            return typeof value.sort === "function";
-        }
-    },
-
-    checkForProperty: function (object, property) {
-        if (property in object) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    },
-
-    invalidRequest: function (config) {
-        return config.MSG_INVALID_VALUE;
-    },
-
-    throwAnError: function () {
-        throw new Exception("This is a custom Exception");
-    },
-
-    throwACustomError: function () {
-        Errors.CustomError.prototype = new Error();
-        throw new Errors.CustomError("My custom error");
-    }
-
-
-
+Application.BookWrapper.prototype.addAuthor = function (author) {
+    "use strict";
+    Application.book.authors.add(author);
 };
 
-var config = {
-    MSG_INVALID_VALUE: "Invalid value",
-    URL_INVALID: "/errors/invalid/",
-    CSS_SELECTED: "selected"
-};
+(function () {
+    "use strict";
+    var result;
 
-var Errors = {
+    SecondApplication.foo = function () {
+        bar();
+    };
 
-    CustomError: function CustomError(message) {
-        this.message = message;
+    function bar() {
+        console.log("bar was called");
     }
-}
-
-Application.BookWrapper.prototype.addAuthor = function(author){
-        book.authors.add(author);
-}
-
-
-    var SecondApplication = {};
-    
-    (function () {
-        var result;
-
-        SecondApplication.foo = function () {
-            bar();
-        }
-
-        function bar() {
-            console.log("bar was called");
-        }
-    })();
+} ()
+);
 
 
 
--- MaintainableJavascriptTests/assets/js/tests/qunitTests.js
+++ MaintainableJavascriptTests/assets/js/tests/qunitTests.js
@@ -1,191 +1,198 @@
 ﻿
-module("inline test");
+(function () {
+    module("inline test");
 
-test("A basic test", function () {
-    ok(true, "this test is fine");
-    var value = "hello";
-    equals("hello", value);
-});
+    test("A basic test", function () {
+        ok(true, "this test is fine");
+        var value = "hello";
+        equals("hello", value);
+    });
 
-module("namespaced static function");
+    module("namespaced static function");
 
-test("basic addition", function () {
-    var result = Math.add(1, 2);
-    equals(result, 3);
-});
+    test("Math", function () {
+        //var result = Utils.add(1, 2);
+        equals(Utils.add(1,2), 3, "add");
+        equals(Utils.multiply(2, 2), 4, "multiply");
+        equals(Utils.divide(2, 2), 1, "divide");
+        equals(Utils.Math.add(1, 2), 3, "add using math namespace");
+        equals(Utils.Math.subtract(3, 1), 2, "subtract using math namespace");
+    });
 
 
-module("nested namespaced static function");
+    module("nested namespaced static function");
 
-test("namespace addition", function () {
-    var result = Application.Math.add(1, 2);
-    equals(result, 3);
-});
+    test("namespace addition", function () {
+        var result = Application.Math.add(1, 2);
+        equals(result, 3);
+    });
 
-test("namespace subtraction", function () {
-    var result = Application.Math.subtract(3, 1);
-    equals(result, 2);
-});
+    test("namespace subtraction", function () {
+        var result = Application.Math.subtract(3, 1);
+        equals(result, 2);
+    });
 
-module("Namespaced new object");
+    module("Namespaced new object");
 
-test("can create a new book unit", function () {
-    var result = new Application.Book("War of the Worlds");
-    var name = result.name;
-    equals(name, "War of the Worlds");
-});
+    test("can create a new book unit", function () {
+        var result = new Application.Book("War of the Worlds");
+        var name = result.name;
+        equals(name, "War of the Worlds");
+    });
 
-module("Namespaced object instance / type");
+    module("Namespaced object instance / type");
 
-test("verify the type of an  Book", function () {
-    var result = new Application.Book("Another Book");
-    ok(result instanceof Application.Book);
-});
+    test("verify the type of an  Book", function () {
+        var result = new Application.Book("Another Book");
+        ok(result instanceof Application.Book);
+    });
 
-test("verify that add is a function in the Midway namespace", function () {
-    ok(typeof Application.Math.add === "function");
-});
+    test("verify that add is a function in the Midway namespace", function () {
+        ok(typeof Application.Math.add === "function");
+    });
 
-module("Namespace array comparison");
+    module("Namespace array comparison");
 
-test("is array", function () {
-    var anArray = [0, 1, 2, 3];
-    ok(Application.Comparison.isArray(anArray));
-});
+    test("is array", function () {
+        var anArray = [0, 1, 2, 3];
+        ok(Application.Comparison.isArray(anArray));
+    });
 
-test("is not an array", function () {
-    var notAnArray = 2;
-    ok(!Application.Comparison.isArray(notAnArray));
-});
+    test("is not an array", function () {
+        var notAnArray = 2;
+        ok(!Application.Comparison.isArray(notAnArray));
+    });
 
-module("object property");
+    module("object property");
 
-test("check on object for a property", function () {
-    var object = new Application.Book();
-    ok(Application.checkForProperty(object, "name"));
-});
+    test("check on object for a property", function () {
+        var object = new Application.Book();
+        ok(Application.checkForProperty(object, "name"));
+    });
 
-module("configuration");
+    module("Application configuration");
 
-test("return a config message", function () {
-    var result = Application.invalidRequest(config);
-    equal(result, "Invalid value");
-});
+    test("return a config message", function () {
+        var result = Application.invalidRequest(Application.config);
+        equal(result, "Invalid value");
+    });
 
-module("error handling");
+    module("error handling");
 
-test("throws an error", function () {
-    raises(function () {
-        Application.throwAnError();
-    }, Error, "Must throw error to pass.");
-});
+    test("throws an error", function () {
+        raises(function () {
+            Application.throwAnError();
+        }, Error, "Must throw error to pass.");
+    });
 
-test("throws a custom custom error", function () {
-    raises(function () {
-        Application.throwACustomError();
-    }, Error, "Must throw an error to pass.");
-});
+    test("throws a custom custom error", function () {
+        raises(function () {
+            Application.throwACustomError();
+        }, Error, "Must throw an error to pass.");
+    });
 
-test("check the error type of custom error", function () {
-    try {
-        Application.throwACustomError();
-    } catch (ex) {
-        ok(ex instanceof Errors.CustomError)
-    }
-});
+    test("check the error type of custom error", function () {
+        try {
+            Application.throwACustomError();
+        } catch (ex) {
+            ok(ex instanceof Errors.CustomError)
+        }
+    });
 
-module("inheritance");
+    module("inheritance");
 
-test("object-based inheritance", function () {
-    var person = Object.create(Application.Person);
-    equal(person.sayName(), "James");
-});
+    test("object-based inheritance", function () {
+        var person = Object.create(Application.Person);
+        equal(person.sayName(), "James");
+    });
 
-test("object-based inheritance with override", function () {
-    var person1 = Object.create(Application.Person);
-    var person2 = Object.create(Application.Person, { name: { value: "Bob"} });
-    equal(person1.sayName(), "James");
-    equal(person2.sayName(), "Bob");
+    test("object-based inheritance with override", function () {
+        var person1 = Object.create(Application.Person);
+        var person2 = Object.create(Application.Person, { name: { value: "Bob"} });
+        equal(person1.sayName(), "James");
+        equal(person2.sayName(), "Bob");
 
-});
+    });
 
-test("type-based inheritance", function () {
-    //var book = new Application.Book("The Man and the Sea");
-    //var book = new Application.Book();
-    Application.HeavyBook.prototype = new Application.Book();
-    equal(Application.HeavyBook.name, "");
-});
+    test("type-based inheritance", function () {
+        //var book = new Application.Book("The Man and the Sea");
+        //var book = new Application.Book();
+        Application.HeavyBook.prototype = new Application.Book();
+        equal(Application.HeavyBook.name, "");
+    });
 
-test("facade pattern", function () {
-    var book = new Application.Book("Lord of the Flies");
-    var author = {};
-    author.firstname = "William";
-    author.lastname = "Golding";
-    var bookWrapper = new Application.BookWrapper(book);
-    //bookWrapper.addAuthor(author); //need to read up on this one, not sure why it isn't working....
-});
+    test("facade pattern", function () {
+        var book = new Application.Book("Lord of the Flies");
+        var author = {};
+        author.firstname = "William";
+        author.lastname = "Golding";
+        var bookWrapper = new Application.BookWrapper(book);
+        //bookWrapper.addAuthor(author); //need to read up on this one, not sure why it isn't working....
+    });
 
-test("object is not extensible", function () {
-    var person = Application.Person;
-    var book = new Application.Book("A book");
-    Object.preventExtensions(person);
-    Object.preventExtensions(book);
-    ok(!Object.isExtensible(person));
-    ok(!Object.isExtensible(book));
-    book.author = "An Author";
-    notEqual(book.name, "An Author");
-});
+    test("object is not extensible", function () {
+        var person = Application.Person;
+        var book = new Application.Book("A book");
+        Object.preventExtensions(person);
+        Object.preventExtensions(book);
+        ok(!Object.isExtensible(person));
+        ok(!Object.isExtensible(book));
+        book.author = "An Author";
+        notEqual(book.name, "An Author");
+    });
 
-test("object is sealed", function () {
-    "use strict";
-    var person = Application.Person;
-    Object.seal(person);
-    ok(!Object.isExtensible(person));
-    ok(Object.isSealed(person));
+    test("object is sealed", function () {
+        "use strict";
+        var person = Application.Person;
+        Object.seal(person);
+        ok(!Object.isExtensible(person));
+        ok(Object.isSealed(person));
 
-    raises(function () {
-        delete person.name;
-    },
+        raises(function () {
+            delete person.name;
+        },
         Error, "Should not be able to delete propery of sealed object");
-    raises(function () {
-        person.age = 25;
-    }, Error, "Should not be able to modify a non existent property");
+        raises(function () {
+            person.age = 25;
+        }, Error, "Should not be able to modify a non existent property");
 
-});
+    });
 
-test("object is frozen", function () {
-    "use strict";
-    var person = Application.Person;
-    Object.freeze(person);
-    ok(!Object.isExtensible(person));
-    ok(Object.isSealed(person));
-    ok(Object.isFrozen(person));
-    raises(function () {
-        person.name = "Greg";
-    }, Error, "Cannot set an existing property on a frozen object");
-});
+    test("object is frozen", function () {
+        "use strict";
+        var person = Application.Person;
+        Object.freeze(person);
+        ok(!Object.isExtensible(person));
+        ok(Object.isSealed(person));
+        ok(Object.isFrozen(person));
+        raises(function () {
+            person.name = "Greg";
+        }, Error, "Cannot set an existing property on a frozen object");
+    });
 
-test("namespaced function can call the bar function through foo", function () {
-    //"use strict";
-    //var app = Object.create(SecondApplication);
-    var app = new Object(SecondApplication);
-    var app2 = Object.create(SecondApplication);
-    try {
-        app.foo();
-        app2.foo();
-        SecondApplication.foo();
-        ok(true, "application object is instantiated and hidden functio bar is called through foo");
-    } catch (ex) {
-        ok(false);
-    }
+    test("namespaced function can call the bar function through foo", function () {
+        //"use strict";
+        //var app = Object.create(SecondApplication);
+        var app = new Object(SecondApplication);
+        var app2 = Object.create(SecondApplication);
+        try {
+            app.foo();
+            app2.foo();
+            SecondApplication.foo();
+            ok(true, "application object is instantiated and hidden functio bar is called through foo");
+        } catch (ex) {
+            ok(false);
+        }
 
-    try {
-        SecondApplication.bar();
-        ok(false);
-    } catch (e) {
-        ok(true, "directly calling bar should fail");
-    }
+        try {
+            SecondApplication.bar();
+            ok(false);
+        } catch (e) {
+            ok(true, "directly calling bar should fail");
+        }
 
-});
+    });
+} ()
+);
 
 
